601dd501ec0f426d1ed14f5065b48bf7ae62ef91
Arun T <arun@laptop>
2018-07-09 17:03:43 +0530

    creating repos

--- AE/Manual/script/eval_ser_data.sh
+++ AE/Manual/script/eval_ser_data.sh
@@ -24,23 +24,24 @@ scp ./script/ser_data.sh $HOME_PATH
 
 
 if [ "$BENCHMARK" = "all" ]; then
-	$BASHPATH ser_data.sh "bfsBellmanFord" $HOME_PATH > ./Results/data_serialized/all_kernels_data_serialized.txt
-	$BASHPATH ser_data.sh "bfsDijkstra" $HOME_PATH >> ./Results/data_serialized/all_kernels_data_serialized.txt
-	$BASHPATH ser_data.sh "byzantine" $HOME_PATH >> ./Results/data_serialized/all_kernels_data_serialized.txt
-	$BASHPATH ser_data.sh "dijkstraRouting" $HOME_PATH >> ./Results/data_serialized/all_kernels_data_serialized.txt
-	$BASHPATH ser_data.sh "dominatingSet" $HOME_PATH >> ./Results/data_serialized/all_kernels_data_serialized.txt	
-	$BASHPATH ser_data.sh "kcommitte" $HOME_PATH >> ./Results/data_serialized/all_kernels_data_serialized.txt
-	$BASHPATH ser_data.sh "leader_elect_dp" $HOME_PATH >> ./Results/data_serialized/all_kernels_data_serialized.txt
-	$BASHPATH ser_data.sh "leader_elect_hs" $HOME_PATH >> ./Results/data_serialized/all_kernels_data_serialized.txt
-	$BASHPATH ser_data.sh "leader_elect_lcr" $HOME_PATH >> ./Results/data_serialized/all_kernels_data_serialized.txt
-	$BASHPATH ser_data.sh "mis" $HOME_PATH >> ./Results/data_serialized/all_kernels_data_serialized.txt
-	$BASHPATH ser_data.sh "mst" $HOME_PATH >> ./Results/data_serialized/all_kernels_data_serialized.txt
-	$BASHPATH ser_data.sh "vertexColoring" $HOME_PATH >> ./Results/data_serialized/all_kernels_data_serialized.txt
+	$BASHPATH ser_data.sh "bfsBellmanFord" $HOME_PATH | tee ./Results/data_serialized/all_kernels_data_serialized.txt
+	$BASHPATH ser_data.sh "bfsDijkstra" $HOME_PATH | tee -a ./Results/data_serialized/all_kernels_data_serialized.txt
+	$BASHPATH ser_data.sh "byzantine" $HOME_PATH | tee -a ./Results/data_serialized/all_kernels_data_serialized.txt
+	$BASHPATH ser_data.sh "dijkstraRouting" $HOME_PATH | tee -a ./Results/data_serialized/all_kernels_data_serialized.txt
+	$BASHPATH ser_data.sh "dominatingSet" $HOME_PATH | tee -a ./Results/data_serialized/all_kernels_data_serialized.txt	
+	$BASHPATH ser_data.sh "kcommitte" $HOME_PATH | tee -a ./Results/data_serialized/all_kernels_data_serialized.txt
+	$BASHPATH ser_data.sh "leader_elect_dp" $HOME_PATH | tee -a ./Results/data_serialized/all_kernels_data_serialized.txt
+	$BASHPATH ser_data.sh "leader_elect_hs" $HOME_PATH | tee -a ./Results/data_serialized/all_kernels_data_serialized.txt
+	$BASHPATH ser_data.sh "leader_elect_lcr" $HOME_PATH | tee -a ./Results/data_serialized/all_kernels_data_serialized.txt
+	$BASHPATH ser_data.sh "mis" $HOME_PATH | tee -a ./Results/data_serialized/all_kernels_data_serialized.txt
+	$BASHPATH ser_data.sh "mst" $HOME_PATH | tee -a ./Results/data_serialized/all_kernels_data_serialized.txt
+	$BASHPATH ser_data.sh "vertexColoring" $HOME_PATH | tee -a ./Results/data_serialized/all_kernels_data_serialized.txt
 elif [ "$BENCHMARK" = "bfsBellmanFord" ] || [ "$BENCHMARK" = "bfsDijkstra" ] || [ "$BENCHMARK" = "dominatingSet" ] || [ "$BENCHMARK" = "kcommitte" ] || [ "$BENCHMARK" = "leader_elect_dp" ] || 	[ "$BENCHMARK" = "mis" ] || [ "$BENCHMARK" = "mst" ] || [ "$BENCHMARK" = "dijkstraRouting" ] || [ "$BENCHMARK" = "vertexColoring" ] || [ "$BENCHMARK" = "byzantine" ] || [ "$BENCHMARK" = "leader_elect_hs" ] || [ "$BENCHMARK" = "leader_elect_lcr" ]; then
 	FILE_TEMP1=$BENCHMARK"_data_serialized.txt"
-	$BASHPATH ser_data.sh $BENCHMARK $HOME_PATH > ./Results/data_serialized/$FILE_TEMP1
+	$BASHPATH ser_data.sh $BENCHMARK $HOME_PATH | tee ./Results/data_serialized/$FILE_TEMP1
 else 
 	echo "Benchmark name is incorrect"
 fi
 
+echo "The generated results will be available at folder ATHOME/Results"
 rm ser_data.sh
